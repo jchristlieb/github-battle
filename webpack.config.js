6d3246e66132581d0452cbe5a1ca7e6da00a4c67
jchristlieb <mail@janchristlieb.de>
2020-10-21 07:27:56 +0200

    prepare production build mode

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
             {test: /\.(css)$/, use: ['style-loader', 'css-loader']}
         ]
     },
-    mode: 'development',
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     plugins: [
         new HtmlWebpackPlugin({
             template: "app/index.html"
